1722bccf78bf5c3912fabd73a62a239958432491
WebMorda <artemenkov.igor@gmail.com>
2021-09-22 18:59:50 +0300

    lesson8

---
+++ java/src/javacourse/lesson8/Book.java
@@ -0,0 +1,29 @@
+package javacourse.lesson8;
+
+public class Book {
+    private String name;
+    private Author author;
+    private int yearPublisher;
+
+    public Book(String name, Author author, int yearPublisher) {
+        this.name = name;
+        this.yearPublisher = yearPublisher;
+        this.author = new Author(author.getFirstName(), author.getLastName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearPublisher() {
+        return yearPublisher;
+    }
+
+    public void setYearPublisher(int yearPublisher) {
+        this.yearPublisher = yearPublisher;
+    }
+}
