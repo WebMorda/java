5bacb256b43508e2b14c5db237a5868d03ea551f
WebMorda <artemenkov.igor@gmail.com>
2021-10-25 15:36:59 +0300

    part 2 lesson 5

---
+++ java/part2.lesson5/src/main/java/pro/sky/part2/lesson5/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package pro.sky.part2.lesson5.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
---
+++ java/part2.lesson5/src/main/java/pro/sky/part2/lesson5/services/IEmployeeService.java
@@ -0,0 +1,12 @@
+package pro.sky.part2.lesson5.services;
+
+import pro.sky.part2.lesson5.models.Employee;
+
+import java.util.List;
+
+public interface IEmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+    String removeEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+    List<Employee> getAllEmployee();
+}
